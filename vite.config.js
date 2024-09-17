a8adec29dc23ab75b54dea363b890212839610d7
OptimasXAnimesh <optimasprime144@gmail.com>
2024-09-17 11:43:01 +0530

    add file

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/cpucsgProject/',  // Set this to match your GitHub Pages repository path
   css: {
-    // Ensure Vite processes CSS correctly
     preprocessorOptions: {
       css: {
-        // Add any specific options here
+        // Add any specific options here if needed
       },
     },
   },
